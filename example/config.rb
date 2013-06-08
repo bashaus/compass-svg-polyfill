bd9f61a2dfff5cd8afe5c4ed45048ad05fbc4bcb
Bashkim Isai <bashkim.isai@agencyrepublic.com>
2013-06-08 18:44:14 +0100

    removed http_images_dir as it's not correct

--- example/config.rb
+++ example/config.rb
@@ -9,8 +9,6 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-http_images_dir = "images/"
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
